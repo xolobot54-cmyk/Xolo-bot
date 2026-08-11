9bd478a115c1da9a400d8ea53f6ecd2cbf9fb57e
xolobot54-cmyk <xolobot54@gmail.com>
2026-08-11 03:42:49 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-self.addEventListener('install', e=> self.skipWaiting());
-self.addEventListener('fetch', e=> e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
+self.addEventListener('install', e=>{self.skipWaiting()});
+self.addEventListener('activate', e=>{e.waitUntil(clients.claim())});
